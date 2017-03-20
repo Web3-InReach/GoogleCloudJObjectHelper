07ec8eb8e90b3876fa19f08433320c467ef6c9d9
yohaiag <vbyohai@gmail.com>
2017-03-20 16:45:44 +0200

    GoogleCloudJObjectHelper To Main
    Upload First Version

---
+++ JObjectUtils.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Cloud.Datastore.V1;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json.Linq;
+using Type = System.Type;
+using Value = Google.Cloud.Datastore.V1.Value;
+
+namespace GoogleCloudJObjectHelper
+{
+    public static class JObjectGoogleUtils
+    {
+
+        private static readonly Dictionary<Type,Func<dynamic,Value>> FactoryTypeToGoogle = new Dictionary<Type, Func<dynamic, Value>>
+        {
+            {typeof(double), x=>  new Value { DoubleValue = (double)x} },
+            {typeof(int), x=> new Value { IntegerValue = (long)x} },
+            {typeof(long), x=> new Value { IntegerValue = (long)x} },
+            {typeof(string), x=> new Value { StringValue = (string)x} },
+            {typeof(DateTime), x=> new Value { TimestampValue = Timestamp.FromDateTime((DateTime)x)} },
+            {typeof(bool), x=> new Value { BooleanValue = (bool)x} },
+            {typeof(NullValue), x=> new Value { NullValue  = NullValue.NullValue} },
+            {typeof(Entity), x=> new Value { EntityValue  = (Entity)x} }
+        };
+
+
+        private static readonly Dictionary<Type, Func<IEnumerable, Value>> FactoryArrayTypeToGoogle = new Dictionary<Type, Func<IEnumerable, Value>>
+        {
+            {typeof(double), x=>  new Value { ArrayValue = ConvertToArray<double>(x) } },
+            {typeof(int), x=> new Value { ArrayValue = ConvertToArray<long>(x)} },
+            {typeof(long), x=> new Value { ArrayValue = ConvertToArray<long>(x)} },
+            {typeof(string), x=> new Value { ArrayValue = ConvertToArray<string>(x)} },
+            {typeof(DateTime), x=> new Value { ArrayValue = ConvertToArray<DateTime>(x)}},
+            {typeof(bool), x=> new Value { ArrayValue = ConvertToArray<bool>(x)} },
+            {typeof(Entity), x=> new Value { ArrayValue  = ConvertToArray<Entity>(x)} },
+            {typeof(Value), x=> new Value { ArrayValue  = ConvertToArray<Value>(x)} },
+            {typeof(IEnumerable), x=> new Value { ArrayValue  = ConvertToArray<ArrayValue>(x)} }
+
+
+        };
+
+
+        public static Entity ConvertToEntity(this JObject jobject)
+        {
+            var entityToReturn = new Entity();
+            ConvertEntityRecursion(jobject, entityToReturn);
+            return entityToReturn;
+        }
+
+
+        private static void ConvertEntityRecursion(this JObject jobject, Entity entity)
+        {
+
+            jobject.Properties().ToList().ForEach(x =>
+            {
+                if (x.Value.Type == JTokenType.Object)
+                {
+                    AddInnerJObject(entity, x);
+                }
+                else if (x.Value.Type == JTokenType.Array)
+                {
+                    IList<dynamic> listEntities = new List<dynamic>();
+                    BuildJArray(x, listEntities);
+                    if (listEntities.First() is Entity)
+                        entity.Properties.Add(x.Name, listEntities.Cast<Entity>().ToArray());
+                    else
+                        entity.Properties.Add(x.Name, listEntities.Cast<Value>().ToArray());
+
+                }
+                else
+                {
+                    
+                    entity.Properties.Add(x.Name, ConvertToGoogleValue(((JValue)x.Value).Value));
+                }
+            });
+        }
+
+        private static void BuildJArray(JToken x,ICollection<dynamic> listEntities)
+        {
+
+
+
+            var tmpValue = x.Value<dynamic>();
+            JArray arr;
+
+            if (tmpValue is JProperty)
+                arr = (JArray)((JProperty)tmpValue).Value;
+            else
+                arr = x.Value<JArray>();
+            arr.ToList().ForEach(r =>
+            {
+                if (r.Type == JTokenType.Object)
+                {
+                    AddJObjectToArray(listEntities, r);
+                }
+                else if (r.Type == JTokenType.Array)
+                {
+                    AddArrayToArray(listEntities, r);
+                }
+                else
+                {
+                    listEntities.Add(ConvertToGoogleValue(r.Value<JValue>().Value));
+                }
+            });
+        }
+
+        private static void AddArrayToArray(ICollection<dynamic> listEntities, JToken r)
+        {
+            IList<dynamic> innerlistEntities = new List<dynamic>();
+            BuildJArray(r, innerlistEntities);
+            listEntities.Add(ConvertToGoogleArrayValue(innerlistEntities.ToArray()));
+        }
+
+        private static void AddJObjectToArray(ICollection<dynamic> listEntities, JToken r)
+        {
+            var entityToAdd = new Entity();
+            ConvertEntityRecursion(r.Value<JObject>(), entityToAdd);
+            listEntities.Add(entityToAdd);
+        }
+
+        private static void AddInnerJObject(Entity entity, JProperty x)
+        {
+            var entityToAdd = new Entity();
+            ConvertEntityRecursion((JObject) x.Value, entityToAdd);
+            entity.Properties.Add(x.Name, entityToAdd);
+        }
+
+
+        private static Value ConvertToGoogleValue(dynamic value)
+        {
+            return FactoryTypeToGoogle[value.GetType()](value);
+        }
+
+
+        private static Value ConvertToGoogleArrayValue(IEnumerable<dynamic> value)
+        {
+            return FactoryArrayTypeToGoogle[value.First().GetType()](value);
+        }
+
+        private static T[] ConvertToArray<T>(IEnumerable x)
+        {
+            return x.OfType<T>().ToArray();
+        }
+    }
+}
---
+++ Tests/DataStoreClientTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Cloud.Datastore.V1;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace GoogleCloudJObjectHelper.Tests
+{
+
+    [TestFixture]
+    public class DataStoreClientTests
+    {
+        public static DateTime NowUtc = DateTime.UtcNow;
+
+        [Test]
+        public void TestSimpleJsonToEntityConverter()
+        {
+            var simple = new SimpleJson();
+            var obj = JObject.FromObject(simple);
+            var entity = obj.ConvertToEntity();
+            AssertSimpleJson(entity);
+        }
+
+        private static void AssertSimpleJson(Entity entity)
+        {
+            Assert.True(entity.Properties.Count == 5);
+            Assert.True(entity.Properties["Name"].StringValue == "David");
+            Assert.True(entity.Properties["Age"].IntegerValue == 30);
+            Assert.True(entity.Properties["IsAccepted"].BooleanValue);
+            Assert.True(entity.Properties["LastChecked"].TimestampValue.Equals(Timestamp.FromDateTime(NowUtc)));
+            Assert.True(entity.Properties["Price"].DoubleValue == 65.54);
+        }
+
+
+        [Test]
+        public void TestComplicatedJsonToEntityConverter()
+        {
+            var Complicated = new ComplicatedJson();
+            var obj = JObject.FromObject(Complicated);
+            var entity = obj.ConvertToEntity();
+            AssetComplicated(entity);
+        }
+
+        private static void AssetComplicated(Entity entity)
+        {
+            Assert.True(entity.Properties.Count == 7);
+            AssertSimpleJson((Entity) entity.Properties["simple1"]);
+            AssertSimpleJson((Entity) entity.Properties["simple2"]);
+        }
+
+
+        [Test]
+        public void TestComplicatedJsonWithSimpleArrayValues()
+        {
+            var ComplicatedWithArray = new ComplicatedJsonWithSimpleArray();
+            var obj = JObject.FromObject(ComplicatedWithArray);
+            var entity = obj.ConvertToEntity();
+
+            Assert.True(entity.Properties.Count == 9);
+            Assert.True(entity.Properties["simpleString"].ArrayValue.Values[0].StringValue == "a");
+            Assert.True(entity.Properties["simpleString"].ArrayValue.Values[1].StringValue == "b");
+            Assert.True(entity.Properties["simpleString"].ArrayValue.Values[2].StringValue == "c");
+            Assert.True(entity.Properties["SimpleDateTimes"].ArrayValue.Values[0].TimestampValue.Equals(Timestamp.FromDateTime(NowUtc)));
+        }
+
+
+
+
+
+        [Test]
+        public void TestComplicatedJsonWithArrayToEntityConverter()
+        {
+            var complicatedWithArray = new ComplicatedJsonWithComplexArray();
+            var obj = JObject.FromObject(complicatedWithArray);
+            var entity = obj.ConvertToEntity();
+
+
+            Assert.True(entity.Properties.Count == 12);
+            AssertSimpleJson((Entity)entity.Properties["simple11"].ArrayValue.Values[0]);
+            AssertSimpleJson((Entity)entity.Properties["simple11"].ArrayValue.Values[1]);
+            AssetComplicated((Entity)entity.Properties["simple12"].ArrayValue.Values[0]);
+            AssetComplicated((Entity)entity.Properties["simple12"].ArrayValue.Values[1]);
+            AssetComplicated(entity.Properties["lst"].ArrayValue.Values[0].ArrayValue.Values[0].ArrayValue.Values[0].EntityValue);
+        }
+
+
+
+    }
+
+
+
+
+
+
+    public class SimpleJson
+    {
+        public string Name { get; set; } = "David";
+        public int Age { get; set; } = 30;
+        public bool IsAccepted { get; set; } = true;
+        public DateTime LastChecked { get; set; } = DataStoreClientTests.NowUtc;
+        public Double Price { get; set; } = 65.54;
+    }
+
+
+    public class ComplicatedJson : SimpleJson
+    {
+        public SimpleJson simple1 { get; set; } = new SimpleJson();
+        public SimpleJson simple2 { get; set; } = new SimpleJson();
+
+
+    }
+
+    public class ComplicatedJsonWithSimpleArray : ComplicatedJson
+    {
+        public List<string> simpleString = new List<string> { "a","b","c"};
+        public List<DateTime> SimpleDateTimes = new List<DateTime> { DataStoreClientTests.NowUtc };
+
+
+
+    }
+
+    public class ComplicatedJsonWithComplexArray : ComplicatedJsonWithSimpleArray
+    {
+        public List<SimpleJson> simple11 { get; set; } = new List<SimpleJson> { new SimpleJson(), new SimpleJson() };
+        public List<ComplicatedJson> simple12 { get; set; } = new List<ComplicatedJson> { new ComplicatedJson(), new ComplicatedJson() };
+
+
+        public List<List<List<ComplicatedJson>>> lst = new List<List<List<ComplicatedJson>>> {  new List<List<ComplicatedJson>> { new List<ComplicatedJson> { new ComplicatedJson() } } };
+
+    }
+}
